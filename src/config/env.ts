64a9a6175f4a5f0b9aa5e671610c61b9315dfeb5
Facundo Fenandez <ffernandez951@gmail.com>
2026-04-21 19:53:37 -0300

    cambios login, flujo funcionando con back-end

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,5 +1,5 @@
 export const API_URL_LOCALHOST =
-    process.env.EXPO_PUBLIC_API_URL || "http://localhost:5224"; //TESTING
+    process.env.EXPO_PUBLIC_API_URL || "http://localhost:5224/api/auth"; //TESTING
 
 export const API_URL_PROD =
     process.env.EXPO_PUBLIC_API_URL_PROD || "https://api.nutriflow.com"; //PRODUCCION
